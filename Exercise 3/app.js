6bd02db00f80fb6b212d646c235e6c2f849c345d
Wesley Ren <wren@walmartlabs.com>
2017-07-10 15:50:43 -0700

    updated

--- Exercise 3/app.js
+++ Exercise 3/app.js
@@ -2,5 +2,20 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+          result: function() {
+            return (this.value > 37) ? "done" : "not there yet";
+          }
+        },
+        watch: {
+          value: function(v) {
+            var vm = this;
+            if (vm.value > 37) {
+              setTimeout(function() {
+                vm.value = 0;
+              }, 2000);
+            }
+          }
         }
     });

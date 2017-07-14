03e5001ffc4eef9d3127cb31a5fcd231368e941c
Wesley Ren <wren@walmartlabs.com>
2017-07-14 14:01:13 -0700

    added dynamic component

--- Section_9_dynamic_component/src/App.vue
+++ Section_9_dynamic_component/src/App.vue
@@ -1,27 +1,44 @@
 <template>
   <div id="app">
-    <quote>
+    <button @click="selectedComponent = 'appQuote'">Quote</button>
+    <button @click="selectedComponent = 'appAuthor'">Author</button>
+    <button @click="selectedComponent = 'appNew'">New Quote</button>
+    <p>
+      {{ selectedComponent }}
+    </p>
+    <keep-alive>
+      <component :is="selectedComponent">
+        <p>Default Content</p>
+      </component>
+    </keep-alive>
+
+    <!--
+    <app-quote>
       <h2 slot="title">{{ quoteTitle }}</h2>
       <hr />
       <p slot="content">My content</p>
-      <p>
-        no slot name
-      </p>
-    </quote>
+      <p>no slot name</p>
+    </app-quote>
+    -->
   </div>
 </template>
 
 <script>
 import Quote from './components/Quote.vue';
+import Author from './components/Author.vue';
+import New from './components/New.vue';
 
 export default {
   data: function() {
     return {
-      quoteTitle: "my Title"
+      quoteTitle: "my Title",
+      selectedComponent: "appQuote"
     }
   },
   components: {
-    'quote': Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 }
 </script>
---
+++ Section_9_dynamic_component/src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>
+      New quote
+    </p>
+    <button @click="counter++">Increase counter</button>
+    <p>
+      {{ counter }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  destroyed() {
+    console.log('New is destroyed')
+  },
+  deactivated() {
+    console.log('New is deactivated')
+  },
+  activated() {
+    console.log('New is activated')
+  }
+}
+</script>

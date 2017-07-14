eaeb0b236b4443a04c3195a5528637969355b262
Wesley Ren <wren@walmartlabs.com>
2017-07-14 13:37:28 -0700

    added, with slot demo

---
+++ Section_9_dynamic_component/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <quote>
+      <h2 slot="title">{{ quoteTitle }}</h2>
+      <hr />
+      <p slot="content">My content</p>
+      <p>
+        no slot name
+      </p>
+    </quote>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+export default {
+  data: function() {
+    return {
+      quoteTitle: "my Title"
+    }
+  },
+  components: {
+    'quote': Quote
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Section_9_dynamic_component/src/components/Quote.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <slot name='title'></slot>
+    <slot name='subtitle'><p>my subtitle</p></slot>
+    <slot name='content'></slot>
+    <slot></slot> <!-- default slot match to default slot -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>

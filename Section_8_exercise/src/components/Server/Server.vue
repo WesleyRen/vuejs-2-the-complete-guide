b3a41607cd4fdb0d37d77010dcb4b059fc6f1a3b
Wesley Ren <wren@walmartlabs.com>
2017-07-13 21:05:18 -0700

    updated to have a separated component for server line item

---
+++ Section_8_exercise/src/components/Server/Server.vue
@@ -0,0 +1,23 @@
+<template>
+  <li
+          class="list-group-item"
+          @click="updateServerDetail(id, status)">
+      Server #{{ id }}, status: {{ status }}
+  </li>
+</template>
+
+<script>
+import { eventBus } from '../../main';
+
+export default {
+  props: {
+    id: Number,
+    status: String
+  },
+  methods: {
+    updateServerDetail(id, status) {
+      eventBus.serverClicked(id, status);
+    }
+  }
+}
+</script>
--- Section_8_exercise/src/components/Server/Servers.vue
+++ Section_8_exercise/src/components/Server/Servers.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group" :servers="servers">
-            <li
-                    class="list-group-item"
-                    v-for="server in servers"
-                    @click="updateServerDetail(server.id, server.status)">
-                Server #{{ server.id }}, status: {{ server.status }}
-            </li>
+          <app-server v-for="server in servers"
+            :id="server.id" :status="server.status">
+          </app-server>
         </ul>
     </div>
 </template>
 
 <script>
 import { eventBus } from '../../main';
+import Server from './Server.vue';
 
 export default {
   data: function() {
@@ -25,20 +23,18 @@ export default {
       ]
     };
   },
-  methods: {
-    updateServerDetail(id, status) {
-      eventBus.serverClicked(id, status);
-    }
+  components: {
+    appServer: Server
   },
   created() {
     eventBus.$on('serverStatusWasReset', (data) => {
       for (var i in this.servers) {
-        console.log(i + ' - ' + data.id)
+        console.log('index: ' + i + ' - ' + this.servers[i].id  + ' - ' + data.id)
         if (this.servers[i].id == data.id) {
           this.servers[i].status = data.status;
         }
       }
-    })
+    });
   }
 }
 </script>

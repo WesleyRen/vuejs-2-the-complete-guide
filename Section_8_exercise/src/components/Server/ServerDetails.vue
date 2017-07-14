0c734fe493fd62a453b734835dd4c3e175fa5a0f
Wesley Ren <wren@walmartlabs.com>
2017-07-13 20:06:39 -0700

    done

--- Section_8_exercise/src/components/Server/ServerDetails.vue
+++ Section_8_exercise/src/components/Server/ServerDetails.vue
@@ -1,11 +1,36 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+    <div class="col-xs-12 col-sm-6" v-if="serverId != null">
+      <p>
+        Server: {{ serverId }} was selected, with status: {{ status }}.
+      </p>
+      <button @click="resetServerStatus">Reset Server Status</button>
     </div>
-
 </template>
 
 <script>
+import { eventBus } from '../../main';
+
+export default {
+  data: function() {
+    return {
+      serverId: null,
+      status: null
+    }
+  },
+  methods: {
+    resetServerStatus() {
+      this.status = 'Normal';
+      var id = this.serverId, status = this.status;
+      eventBus.$emit('serverStatusWasReset', {id, status});
+    }
+  },
+  created() {
+    eventBus.$on('serverWasClicked', (serverInfo) => {
+      this.serverId = serverInfo.id;
+      this.status = serverInfo.status;
+    })
+  }
+}
 </script>
 
 <style>
--- Section_8_exercise/src/components/Server/Servers.vue
+++ Section_8_exercise/src/components/Server/Servers.vue
@@ -1,16 +1,46 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
+        <ul class="list-group" :servers="servers">
             <li
                     class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
+                    v-for="server in servers"
+                    @click="updateServerDetail(server.id, server.status)">
+                Server #{{ server.id }}, status: {{ server.status }}
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+import { eventBus } from '../../main';
+
+export default {
+  data: function() {
+    return {
+      servers: [
+        {id: 1, status: 'Normal'},
+        {id: 2, status: 'Critical'},
+        {id: 3, status: 'Unknown'},
+        {id: 4, status: 'Normal'}
+      ]
+    };
+  },
+  methods: {
+    updateServerDetail(id, status) {
+      eventBus.serverClicked(id, status);
+    }
+  },
+  created() {
+    eventBus.$on('serverStatusWasReset', (data) => {
+      for (var i in this.servers) {
+        console.log(i + ' - ' + data.id)
+        if (this.servers[i].id == data.id) {
+          this.servers[i].status = data.status;
+        }
+      }
+    })
+  }
+}
 </script>
 
 <style>

1a3ebdf98e3b8980de6f9189b4ff2e4139bc2869
Wesley Ren <wren@walmartlabs.com>
2017-07-14 23:35:28 -0700

    done

--- Project_2_guided/src/components/Quote.vue
+++ Project_2_guided/src/components/Quote.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-sm-6 col-md-4 col-lg-3">
-    <div class="panel panel-body panel-default">
+    <div class="panel panel-body panel-default quote">
       <slot></slot>
     </div>
   </div>

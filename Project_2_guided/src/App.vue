e9a678fe377aa1aa6c47a36e32ef4e256cb6c60e
Wesley Ren <wren@walmartlabs.com>
2017-07-14 23:31:00 -0700

    done

--- Project_2_guided/src/App.vue
+++ Project_2_guided/src/App.vue
@@ -1,13 +1,45 @@
 <template>
     <div class="container">
-    
+      <app-progress-bar :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-progress-bar>
+      <app-new-quote @newQuoteCreated="createQuote($event)"></app-new-quote>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote($event)"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">Info: Click on a Quote to delete it.</div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import AppProgressBar from './components/ProgressBar.vue';
+import AppNewQuote from './components/NewQuote.vue';
+import AppQuoteGrid from './components/QuoteGrid.vue';
+
+export default {
+    data: function() {
+        return {
+          quotes: [],
+          maxQuotes: 10
+        };
+    },
+    components: {
+      AppNewQuote,
+      AppProgressBar,
+      AppQuoteGrid
+    },
+    methods: {
+      createQuote(quote) {
+        if (this.quotes.length >= this.maxQuotes) {
+          return alert("You've reached your maximum quota of " + this.maxQuotes + " quotes.")
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
     }
+}
 </script>
 
 <style>
---
+++ Project_2_guided/src/components/NewQuote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+      <p class="text">Quote</p>
+      <textarea class="form-control" v-model="quote">{{ quote }}</textarea>
+      <div style="text-align: center;">
+        <button class="btn btn-primary" :disabled="!quote" @click="createQuote">Add Quote</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: '',
+    };
+  },
+  methods: {
+    createQuote() {
+      this.$emit('newQuoteCreated', this.quote);
+      this.quote = '';
+    }
+  }
+}
+</script>
+
+<style>
+</style>

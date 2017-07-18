f0bb2853299d6408358c4cad77e5ed1f1a5bca99
Wesley Ren <wren@walmartlabs.com>
2017-07-17 17:09:44 -0700

    done

--- Exercise 12_directives/src/App.vue
+++ Exercise 12_directives/src/App.vue
@@ -5,14 +5,48 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <button v-my-on:click="log">click me</button>
+                <hr />
+                <div style="width: 100px; height: 100px; backgroundColor: green"
+                  v-my-on:mouseenter="mouseEnter"
+                  v-my-on:mouseleave="mouseLeave">
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+      data() {
+        return {
+          input1: ''
+        };
+      },
+      directives: {
+        'my-on': {
+          bind(el, binding, vnode) {
+            // el.onclick = function() {
+            //   binding.value();
+            // }
+            const type = binding.arg;
+            const fn = binding.value;
+            el.addEventListener(type, fn);
+          }
+        }
+      },
+      methods: {
+        log() {
+          console.log('button clicked');
+        },
+        mouseEnter() {
+          console.log('mouse entered');
+        },
+        mouseLeave() {
+          console.log('mouse leaved');
+        }
+      }
+    }
 </script>
 
 <style>

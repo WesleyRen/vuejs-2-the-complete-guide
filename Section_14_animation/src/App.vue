562b0c02a951d7847704f739a7caaab850bd9d00
Wesley Ren <wren@walmartlabs.com>
2017-07-22 21:44:41 -0700

    added transition-group animation

--- Section_14_animation/src/App.vue
+++ Section_14_animation/src/App.vue
@@ -62,6 +62,21 @@
                 <transition name="slide" mode="out-in">
                   <component :is="selectedComponent"></component>
                 </transition>
+                <hr />
+
+                <button class="btn btn-primary" @click="addItem">Add Itme</button>
+                <br />
+                <ul class="list-group">
+                  <transition-group name="slide">
+                    <li class="list-group-item"
+                        v-for="(number, index) in numbers"
+                        @click="removeItem(index)"
+                        style="curso: pointer"
+                        :key="number">
+                      {{ number }}
+                    </li>
+                  </transition-group>
+                </ul>
             </div>
         </div>
     </div>
@@ -78,7 +93,8 @@ import SuccessAlert from './SuccessAlert.vue';
               load: true,
               alertAnimation: 'fade',
               elementWidth: 100,
-              selectedComponent: 'success-alert'
+              selectedComponent: 'success-alert',
+              numbers: [1, 2, 3, 4, 5]
             }
         },
         components: {
@@ -133,6 +149,13 @@ import SuccessAlert from './SuccessAlert.vue';
           },
           leaveCancelled(el) {
             console.log('leaveCancelled');
+          },
+          addItem() {
+            const pos = Math.floor(Math.random() * this.numbers.length);
+            this.numbers.splice(pos, 0, this.numbers.length + 1);
+          },
+          removeItem(index) {
+            this.numbers.splice(index, 1);
           }
         },
         computed: {
@@ -175,6 +198,11 @@ import SuccessAlert from './SuccessAlert.vue';
   animation: slide-out 1s ease-out forwards;
   transition: opacity 1s;
   opacity: 0;
+  position: absolute;
+}
+
+.slide-move {
+  transition: transform 1s;
 }
 
 @keyframes slide-in {

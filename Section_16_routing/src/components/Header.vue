8df78276f3a4ca088687fd2babbe0c3b536d4a52
Wesley Ren <wren@walmartlabs.com>
2017-07-24 21:57:39 -0700

    added reactive code to route parameters

--- Section_16_routing/src/components/Header.vue
+++ Section_16_routing/src/components/Header.vue
@@ -2,6 +2,7 @@
   <!-- Get styles from http://getbootstrap.com/components -->
   <ul class="nav nav-pills">
     <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-    <router-link to="/User" tag="li" active-class="active"><a>User</a></router-link>
+    <router-link to="/user/1" tag="li" active-class="active"><a>User</a></router-link>
+    <router-link to="/user/2" tag="li" active-class="active"><a>User</a></router-link>
   </ul>
 </template>

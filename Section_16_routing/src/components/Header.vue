0a738014eb20f234e359f88251b2dc7b652299e5
Wesley Ren <wren@walmartlabs.com>
2017-07-24 22:15:03 -0700

    added navigating to nested routes, with parameters

--- Section_16_routing/src/components/Header.vue
+++ Section_16_routing/src/components/Header.vue
@@ -2,7 +2,6 @@
   <!-- Get styles from http://getbootstrap.com/components -->
   <ul class="nav nav-pills">
     <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-    <router-link to="/user/1" tag="li" active-class="active"><a>User</a></router-link>
-    <router-link to="/user/2" tag="li" active-class="active"><a>User</a></router-link>
+    <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
   </ul>
 </template>
--- Section_16_routing/src/components/user/UserDetail.vue
+++ Section_16_routing/src/components/user/UserDetail.vue
@@ -1,3 +1,8 @@
 <template>
+  <div>
     <h3>Some User Details</h3>
+    <p>
+      User {{ $route.params.id }} loaded
+    </p>
+  </div>
 </template>

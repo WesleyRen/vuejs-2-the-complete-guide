3496a27844b0a92c5d966306d5ebab03ac36a71f
Wesley Ren <wren@walmartlabs.com>
2017-07-24 17:45:30 -0700

    added interceptor for method and response.

--- Section_15_http/src/main.js
+++ Section_15_http/src/main.js
@@ -5,6 +5,15 @@ import App from './App.vue'
 Vue.use(VueResource)
 
 Vue.http.options.root = 'https://http-vuejs-855ab.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST') {
+    request.method = 'PUT'; // put alway overwrite Firebase existing data.
+  }
+  next(response => {
+    response.json = () => {return {messages: response.body} };
+  });
+});
 
 new Vue({
   el: '#app',

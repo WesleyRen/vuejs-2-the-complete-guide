e6d8ed3291f32cbf4cc18072909fa500352a2708
Wesley Ren <wren@walmartlabs.com>
2017-07-24 22:26:43 -0700

    Use named route

--- Section_16_routing/src/components/user/UserDetail.vue
+++ Section_16_routing/src/components/user/UserDetail.vue
@@ -2,7 +2,9 @@
   <div>
     <h3>Some User Details</h3>
     <p>User {{ $route.params.id }} loaded</p>
-    <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'"
+    <router-link
+      tag="button"
+      :to="{ name: 'userEdit', params: {id: $route.params.id} }"
       class="btn btn-primary">Edit User</router-link>
   </div>
 </template>

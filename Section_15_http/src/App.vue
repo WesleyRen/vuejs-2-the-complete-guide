156f554634a69055ee3aa098fdd7dee0143867f1
Wesley Ren <wren@walmartlabs.com>
2017-07-24 18:43:19 -0700

    added code to use a customized resource get function, with customized data node

--- Section_15_http/src/App.vue
+++ Section_15_http/src/App.vue
@@ -17,6 +17,8 @@
         <hr />
         <div class="row">
           <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <label for="input">type in 'data' or 'alternative', which we sent to Firebase earlier.</label>
+            <input class="form-control" type="text" v-model="node" />
             <button class="btn btn-primary" @click="fetchData">Get data</button>
             <ul class="list-group">
               <li class="list-group-item" v-for="u in users">{{ u.name }} , {{ u.email }}</li>
@@ -33,7 +35,8 @@
         return {
           user: {name: '', email: ''},
           users: [],
-          resource: {}
+          resource: {},
+          node: 'data'
         };
       },
       methods: {
@@ -49,7 +52,18 @@
           this.resource.saveAlt(this.user); // customized action.
         },
         fetchData() {
-          this.$http.get('')
+          // this.$http.get('')
+          //   .then(response => {
+          //     return response.json();
+          //   })
+          //   .then(data => {
+          //     console.log(data);
+          //     this.users = [];
+          //     for (let key in data) {
+          //       this.users.push(data[key]);
+          //     }
+          //   });
+          this.resource.getData({node: this.node})
             .then(response => {
               return response.json();
             })
@@ -64,9 +78,10 @@
       },
       created() {
         const customActions = {
-          saveAlt: {method: 'POST', url: 'alternative.json'}
+          saveAlt: {method: 'POST', url: 'alternative.json'},
+          getData: {method: 'GET'}
         };
-        this.resource = this.$resource('data.json', {}, customActions);
+        this.resource = this.$resource('{node}.json', {}, customActions);
       }
     }
 </script>

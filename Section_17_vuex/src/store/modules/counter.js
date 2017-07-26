488d47e93429c307b2f213fa23a52ea6cc9383e6
Wesley Ren <wren@walmartlabs.com>
2017-07-25 23:06:16 -0700

    added modules and separated files

---
+++ Section_17_vuex/src/store/modules/counter.js
@@ -0,0 +1,48 @@
+const state = {
+    counter: 0,
+    clicks: 0
+  };
+const getters = {
+    doubleCounter: state => {
+      return state.counter;
+    },
+    stringCounter: state => {
+      return state.clicks + ' clicks';
+    }
+  };
+const mutations = {
+    increment: (state, payload) => {
+      state.counter += payload;
+      state.clicks++;
+    },
+    decrement: (state, payload) => {
+      state.counter -= payload;
+      state.clicks++;
+    }
+  };
+const actions = {
+    increment: (context, payload) => {
+      context.commit('increment', payload);
+    },
+    decrement: ({ commit }, payload) => { // or only include the needed attribute.
+      commit('decrement', payload);
+    },
+    asyncIncrement: ({ commit }, payload) => {
+      setTimeout(() => {
+        commit('increment', payload.by);
+      }, payload.duration);
+    },
+    asyncDecrement: ({ commit }, payload) => {
+      setTimeout(() => {
+        commit('decrement', payload.by);
+      }, payload.duration);
+    }
+  };
+
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
--- Section_17_vuex/src/store/store.js
+++ Section_17_vuex/src/store/store.js
@@ -1,57 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './modules/counter.js';
+import * as actions from './actions.js';
+import * as getters from './getters.js';
+import * as mutations from './mutations.js';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    counter: 0,
-    clicks: 0,
     value: 0
   },
-  getters: {
-    doubleCounter: state => {
-      return state.counter;
-    },
-    stringCounter: state => {
-      return state.clicks + ' clicks';
-    },
-    value: state => {
-      return state.value;
-    }
-  },
-  mutations: {
-    increment: (state, payload) => {
-      state.counter += payload;
-      state.clicks++;
-    },
-    decrement: (state, payload) => {
-      state.counter -= payload;
-      state.clicks++;
-    },
-    updateValue: (state, payload) => {
-      state.value = payload;
-    }
-  },
-  actions: {
-    increment: (context, payload) => {
-      context.commit('increment', payload);
-    },
-    decrement: ({ commit }, payload) => { // or only include the needed attribute.
-      commit('decrement', payload);
-    },
-    updateValue: ({ commit }, payload) => {
-      commit('updateValue', payload);
-    },
-    asyncIncrement: ({ commit }, payload) => {
-      setTimeout(() => {
-        commit('increment', payload.by);
-      }, payload.duration);
-    },
-    asyncDecrement: ({ commit }, payload) => {
-      setTimeout(() => {
-        commit('decrement', payload.by);
-      }, payload.duration);
-    }
+  getters,
+  mutations,
+  actions,
+  modules: {
+    counter
   }
 })

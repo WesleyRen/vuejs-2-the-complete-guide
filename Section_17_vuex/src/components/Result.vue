8fe9ecf7f960e078115f603e9bb7e800c66e7116
Wesley Ren <wren@walmartlabs.com>
2017-07-25 23:16:37 -0700

    added types files to manage global name space

--- Section_17_vuex/src/components/Result.vue
+++ Section_17_vuex/src/components/Result.vue
@@ -1,13 +1,15 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <p>Counter (doubled) is: {{ counter }}</p>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import * as types from '../store/types';
+
     export default {
       computed: {
-        counter() {
-          return this.$store.getters.doubleCounter;
-        }
+        ...mapGetters({
+          counter: types.DOUBLE_COUNTER,
+        })
       }
-    }
-</script>
+    }</script>
